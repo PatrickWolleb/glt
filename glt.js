40fce79dfd55603a5d6d50372706f6d4d8049aca
PatrickWolleb <patrick.wolleb@shmedia.co.uk>
2013-09-04 10:02:57 -0700

    4 particles working. Using points vertex positions as texture lookup coords

--- glt.js
+++ glt.js
@@ -137,10 +137,11 @@
         return buffer;
     }
 
-    function createTexture(gl) {
+    function createTexture(gl, flipY) {
+        flipY = flipY || false; 
         var t = gl.createTexture();
         gl.bindTexture(gl.TEXTURE_2D, t);
-        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, flipY);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
